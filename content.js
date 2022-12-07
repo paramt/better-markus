f0347bc14394cc4d3518dbf03fb1186c488377e0
Justin <justin106@protonmail.com>
2022-12-07 02:22:45 -0500

    Display average on top of assignments

--- content.js
+++ content.js
@@ -1,4 +1,5 @@
 const RED = "#ff988c";
+const ORANGE = "#FFA07A"
 
 window.onload = function() {
     highlight(); 
@@ -9,11 +10,40 @@ window.onload = function() {
     observer = new MutationObserver(function() {
         highlight();
     });
+	
+	observer2 = new MutationObserver(function() {
+        setAvg();
+    });
 
     // make sure codeholder exists before running observer
-    if(codeholder) { observer.observe(codeholder, {childList: true})};
+    if(codeholder) { 
+		observer.observe(codeholder, {childList: true})
+		observer2.observe(codeholder, {childList: true})
+	};
+	
+	const panes = document.getElementById("panes-content");
+	// let url = chrome.runtime.getURL("./widgets/markbox.html");
+	var markbox = document.createElement("div");
+	markbox.setAttribute("id", "assignmentAvg");
+	// let markbox_text = fetch(url).then(response => response.text())
+								// .then(data=>{markbox.innerHTML = data.trim();})
+								
+	panes.prepend(markbox);
 }
 
+function setAvg() {
+	const marksheet = document.getElementsByClassName("code_scroller")[0];
+    if(!marksheet) return 0;
+
+	for (const child of marksheet.children) {
+		const text = child.children[0].innerHTML.replace(/&nbsp;/g, "");
+		if (text.startsWith("Totalmarksearned")) {
+			let numerator = parseFloat(text.split('/')[1].trim().match(/\d+\.\d+/)[0]);
+			let denominator = parseFloat(text.split('/')[2].match(/\d+\.\d+/)[0]);
+			document.getElementById("assignmentAvg").innerHTML = "<div style='font-size: 1em'><b>Assignment Average: " + ((numerator/denominator)*100).toString() + "</b></div>";
+		}
+	}
+}
 
 function highlight() {
     const marksheet = document.getElementsByClassName("code_scroller")[0];
@@ -35,7 +65,7 @@ function highlight() {
 					child.style.backgroundColor = RED;
 				}
 				else {
-					child.style.backgroundColor = "#FFA07A";
+					child.style.backgroundColor = ORANGE;
 				}
             }
         }
